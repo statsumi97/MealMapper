65f033500c13c19ba39f088ec2a119f128361a8e
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-01-31 09:57:02 -0500

    seed.py and app.py

--- server/app.py
+++ server/app.py
@@ -3,13 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import Flask, make_response, request
 from flask_restful import Resource
+from flask_cors import cross_origin
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import db, User, Restaurant, Memory, Filter
 
 # Views go here!
 
@@ -17,6 +18,38 @@ from config import app, db, api
 def index():
     return '<h1>Project Server</h1>'
 
+## Login Functionality
+@app.route('/login', methods = ['POST'])
+def users_by_email():
+    try:
+        form_data = request.get_json()
+        email = form_data['email']
+        password = form_data['password']
+        user = User.query.filter(User.user_email == email).first()
+
+        if user:
+            if password == user.passwordhash:
+                login_body = user.to_dict(only=('id',))
+                res = make_response(
+                    login_body,
+                    200
+                )
+            else:
+                res = make_response(
+                    {'error': 'wrong password'},
+                    401
+                )
+        else:
+            res = make_response(
+                {'error': 'account does not exist'},
+                404
+            )
+    except:
+        res = make_response(
+            {'error': 'account does not exist'},
+            404
+        )
+    return res
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,45 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Restaurant, Memory, Filter
 
-if __name__ == '__main__':
-    fake = Faker()
-    with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+def create_username():
+    usernames = []
+    emails = []
+
+    user1 = User(
+        username = 'statsumi97',
+        user_email = 'statsumi.sts@gmail.com',
+        passwordhash = 'testing11216'
+    )
+    usernames.append(user1)
+
+    return usernames
+
+def create_restaurants():
+    restaurants = []
+
+    restaurant1 = Restaurant(
+        name = 'Raku',
+        cuisine = 'Japanese',
+        neighborhood = 'East Village',
+        visited = True
+    )
+    restaurants.append(restaurant1)
+
+    if __name__ == '__main__':
+        with app.app_context():
+
+            print('Clearing db...')
+            User.query.delete()
+            Restaurant.query.delete()
+            Memory.query.delete()
+            Filter.query.delete()
+
+            print('Seeding usernames...')
+            usernames = create_username()
+            db.session.add_all(usernames)
+            db.session.commit()
+
+            
+
